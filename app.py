3cac37c590da2b0cc40644c1f7b307bd9fe7b87f
bandara <chathubandara500@gmail.com>
2024-01-29 22:23:02 +0530

    variables input print

--- app.py
+++ app.py
@@ -6,3 +6,18 @@ print("| | | |")
 # muliplying the string
 print("*" * 10)
 
+# variabls
+price = 10
+price = 20
+
+rating = 4.9
+name = "Ian"
+
+is_published = False
+not_published = True
+
+new_price = price*20
+print(new_price)
+
+
+
---
+++ excercices.py
@@ -0,0 +1,15 @@
+ # [01]
+# name = "John Smith"
+# age = 20
+# is_new = True
+
+
+
+# [02]
+#name = input("What is Your Name ?")
+#print("Hi : " + name +" nice To have you here")
+
+# [03]
+name = input("What is your name ?")
+color = input("What is your Fav Color ?")
+print(name +" likes "+ color)
